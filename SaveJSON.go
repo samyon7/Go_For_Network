abad3e61414494e361753113fcdcb2d5ffe7182e
samyon7 <sammyon7@gmail.com>
2022-04-27 08:46:50 +0700

    Fix typo

--- SaveJSON.go
+++ SaveJSON.go
@@ -1,42 +1,51 @@
-package main
-
 import (
-	"encoding/json"
-	"fmt"
-	"os"
+        "encoding/json"
+        "fmt"
+        "os"
 )
 
 type Person struct {
-	Name Name
-	Email []Email
+        Name  Name
+        Email []Email
 }
 
 type Name struct {
-	Family string
-	Personal string
+        Family   string
+        Personal string
 }
 
 type Email struct {
-	Kind string
-	Address string
+        Kind    string
+        Address string
 }
 
-func main(){
-	person := Person{
-		Name: Name {Family: "Newmarch", Personal: "Jan"},
-		Email: []Email{Email{Kind: "home", Address: "jan@newmarch.namea"}, Email{Kind: "work", Address: "j.newmarch@boxhill.edu.au"}}
-	}
+func (p Person) String() string {
+        s := p.Name.Personal + " " + p.Name.Family
+        for _, v := range p.Email {
+                s += "\n" + v.Kind + ": " + v.Address
+        }
+        return s
+}
+func main() {
+        var person Person
+        loadJSON("person.json", &person)
 
-	saveJSON("person.json", person)
+        fmt.Println("Person", person.String())
 }
 
-func saveJSON(){
-
+func loadJSON(fileName string, key interface{}) {
+        inFile, err := os.Open(fileName)
+        checkError(err)
+        decoder := json.NewDecoder(inFile)
+        err = decoder.Decode(key)
+        checkError(err)
+        inFile.Close()
 }
 
 func checkError(err error) {
         if err != nil {
                 fmt.Println("Fatal error ", err.Error())
-                os.Exit(1)
+                os.Exit(1)                                                                      
         }
 }
+
